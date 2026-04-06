12dea802c5db1f4067cb49e438c3ab3efb8eed0e
Nelson Lamounier <lamounierleao@gmail.com>
2026-04-06 07:37:07 +0100

    refactor: decommission apps/admin and set apps/site to port 3000

--- apps/admin/src/app/api/test/route.ts
+++
@@ -1,1 +0,0 @@
-export function GET(req) { return Response.json({ url: req.url, nextUrl: req.nextUrl.pathname }) }
--- apps/admin/src/components/resume/index.ts
+++
@@ -1,3 +0,0 @@
-export { ResumeDocument } from './ResumeDocument'
-export { ResumePreview } from './ResumePreview'
-export { ResumeDownloadButton } from './ResumeDownloadButton'
